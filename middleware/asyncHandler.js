0aedada57eb80e4d11d895616eb6ae91787d1478
Harshithavigna <132732139+Harshithavigna@users.noreply.github.com>
2026-06-25 23:36:43 +0530

    Remove unnecessary newline in asyncHandler.js

--- middleware/asyncHandler.js
+++ middleware/asyncHandler.js
@@ -2,5 +2,4 @@
 // avoiding repetitive try/catch blocks in every controller.
 const asyncHandler = (fn) => (req, res, next) =>
   Promise.resolve(fn(req, res, next)).catch(next);
-
 module.exports = asyncHandler;
